5f93e33f0e0536b3f699743f128750a45f640bbc
Christian Panse <cp@fgcz.ethz.ch>
2020-01-15 11:04:52 +0100

    gg=G

--- support/js/internal/EuBIC2020/api.js
+++ support/js/internal/EuBIC2020/api.js
@@ -2,21 +2,21 @@
 // TODO: docu
 
 function queryProtDBGet(peptideseq="SCTLFPQNPNLPPPSTRER", charge="3"){
-  let url = "https://www.proteomicsdb.org/logic/api/getReferenceSpectrum.xsjs?sequence=" + peptideseq + "&mods=Carbamidomethyl@C2&charge=" + charge
+	let url = "https://www.proteomicsdb.org/logic/api/getReferenceSpectrum.xsjs?sequence=" + peptideseq + "&mods=Carbamidomethyl@C2&charge=" + charge
 
 
-  console.log("charge = " + charge)
-  console.log("peptide = " + peptideseq)
+		console.log("charge = " + charge)
+		console.log("peptide = " + peptideseq)
 
-  return d3.json(url, function(data) {console.log(data)})
+		return d3.json(url, function(data) {console.log(data)})
 }
 
 
 function queryProtDBPOst(){
-    url = "https://www.proteomicsdb.org/logic/api/getFragmentationPrediction.xsjs"
+	url = "https://www.proteomicsdb.org/logic/api/getFragmentationPrediction.xsjs"
 
-    query = {"sequence": ["TDLHAFENLEIIR", "TDLHAFENLEIIR", "VTSGSTSTSR", "LASVSVSR", "YVYVADVAAK"], "charge": [2, 3, 2, 2, 2], "ce": [25, 30, 30, 30, 30]}
+		query = {"sequence": ["TDLHAFENLEIIR", "TDLHAFENLEIIR", "VTSGSTSTSR", "LASVSVSR", "YVYVADVAAK"], "charge": [2, 3, 2, 2, 2], "ce": [25, 30, 30, 30, 30]}
 
-     return d3.json(url, {method: "POST", body: JSON.stringify(query)})
+	return d3.json(url, {method: "POST", body: JSON.stringify(query)})
 }
 
