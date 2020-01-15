7e665d4db795287ed1ed106bdfde9b2a33a56b95
Christian Panse <cp@fgcz.ethz.ch>
2020-01-15 11:15:41 +0100

    implemted function from proteomicsDB to ipsa

--- support/js/internal/EuBIC2020/api.js
+++ support/js/internal/EuBIC2020/api.js
@@ -20,3 +20,42 @@ function queryProtDBPOst(){
 	return d3.json(url, {method: "POST", body: JSON.stringify(query)})
 }
 
+
+/// 2020-01-15 CP/TK
+function transform2scope(a){
+
+	try {
+		let intensitiesmax = Math.max(...a['ions'].map(x => x.intensity))
+
+			scope = {
+x: a['ions'].map(x => x.mz),
+   intensities: a['ions'].map(x => x.intensity),
+   percentBasePeak : a['ions'].map(x => (100 * (x.intensity / intensitiesmax))),
+   massError: a['ions'].map(x => 'NA'),
+   colors: a['ions'].map(x => '#000000'),
+   labels: a['ions'].map(x => x.ion + x.number),
+   labelCharges: a['ions'].map(x => x.charge),
+   neutralLosses: [],
+   widths: [],
+   sequence: a['sequence']
+			}
+
+		return scope;
+	} catch(err) {
+		scope = {
+x: [],
+   intensities: [],
+   massError: [],
+   colors: [],
+   labels: [],
+   labelCharges: [],
+   neutralLosses: [],
+   widths: [],
+   sequence: []
+		}
+		return scope;
+	}
+
+}
+
+
