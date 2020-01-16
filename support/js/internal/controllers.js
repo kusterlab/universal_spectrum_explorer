a44195c0b5c7402d7fac7aaaa839b280ee020ad4
Dorfer Viktoria <viktoria.dorfer@fh-hagenberg.at>
2020-01-16 10:26:05 +0100

    added modification to request

--- support/js/internal/controllers.js
+++ support/js/internal/controllers.js
@@ -255,9 +255,17 @@ angular.module("IPSA.spectrum.controller").controller("GraphCtrl", ["$scope", "$
       };
 
       $scope.submittedData = data;
+      $scope.modObject.selectedMods.sort((x,y) => {return x.index > y.index});
+      let modString = "";
+      $scope.modObject.selectedMods.forEach(function(mod) {
+          if (modString != ""){
+              modString +=",";              
+          }
+          modString += mod.name + "@"+mod.site + (mod.index+1);
+      });
 	  
 	  var url2 = "https://www.proteomicsdb.org/logic/api/getFragmentationPrediction.xsjs";
-	  var query = {"sequence": [$scope.peptide.sequence], "charge": [$scope.peptide.precursorCharge], "ce": [25]};
+	  var query = {"sequence": [$scope.peptide.sequence], "charge": [$scope.peptide.precursorCharge], "ce": [25], "mods" : [modString]};
 
       // httpRequest to submit data to processing script. 
       $http.post(url, data)
